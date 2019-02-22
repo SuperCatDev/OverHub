24bf700a2d13a7d0e256b8c7a7b26d59825230e5
Alexey <ultimatum424@gmail.com>
2019-02-22 17:37:23 +0300

    Add fragment_wiki

---
+++ app/src/main/java/com/sc/overhub/view/fragment/WikiFragment.kt
@@ -0,0 +1,14 @@
+package com.sc.overhub.view.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.sc.overhub.R
+
+class WikiFragment : BaseFragment() {
+
+}
