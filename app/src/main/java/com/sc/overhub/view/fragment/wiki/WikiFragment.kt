6d301a37b140a87145d4ca986e229829ddc2b8bc
SuperCatDev <timchenkonikita123@gmail.com>
2019-02-23 20:17:51 +0300

    Added wiki fragment to nvigation graph

--- app/src/main/java/com/sc/overhub/view/fragment/wiki/WikiFragment.kt
+++ app/src/main/java/com/sc/overhub/view/fragment/wiki/WikiFragment.kt
@@ -1,8 +1,19 @@
 package com.sc.overhub.view.fragment.wiki
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sc.overhub.R
 import com.sc.overhub.view.fragment.BaseFragment
 
 class WikiFragment : BaseFragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_wiki, container, false)
+    }
 }
