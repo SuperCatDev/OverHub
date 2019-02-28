1f2b09c66f1ab7564c77b5135f1a1dd5797e9a9a
Alexey <ultimatum424@gmail.com>
2019-02-28 19:32:37 +0300

    Put VIewModel from WIkiHeroFragmnet

--- app/src/main/java/com/sc/overhub/view/fragment/wiki/herosList/hero/tabs/WikiHeroDescriptionFragment.kt
+++ app/src/main/java/com/sc/overhub/view/fragment/wiki/herosList/hero/tabs/WikiHeroDescriptionFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.sc.overhub.R
-import com.sc.overhub.view.fragment.BaseFragment
+import com.sc.overhub.databinding.FragmentWikiHeroOverviewBinding
+import com.sc.overhub.view.fragment.wiki.herosList.hero.WikiHeroFragment
 
-class WikiHeroDescriptionFragment: BaseFragment() {
+class WikiHeroDescriptionFragment: WikiHeroFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view =  inflater.inflate(R.layout.fragment_wiki_hero_overview, container, false)
+        val fragmentBinding = DataBindingUtil.inflate<FragmentWikiHeroOverviewBinding>(
+            inflater, R.layout.fragment_wiki_hero_overview, container, false)
+        val view = fragmentBinding.root
+        fragmentBinding.viewModel = viewModel
+        fragmentBinding.executePendingBindings()
         return view
     }
 
