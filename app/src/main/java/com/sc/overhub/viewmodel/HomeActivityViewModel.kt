d92ad20d7499394fb476e23c423f43ea7df7ac92
NTimchenko <ntimchenko@niisokb.ru>
2019-03-01 12:00:58 +0300

    Some shit this navigations

--- app/src/main/java/com/sc/overhub/viewmodel/HomeActivityViewModel.kt
+++ app/src/main/java/com/sc/overhub/viewmodel/HomeActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.sc.overhub.viewmodel
 
 import android.app.Activity
+import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import androidx.databinding.ObservableInt
@@ -22,19 +23,33 @@ class HomeActivityViewModel(activity: Activity) : ViewModel() {
         BottomNavigationView.OnNavigationItemSelectedListener {
         override fun onNavigationItemSelected(item: MenuItem): Boolean {
             when (item.itemId) {
-                R.id.menu_statistic -> navController.navigate(R.id.statisticsFragment)
-                R.id.menu_tracker -> navController.navigate(R.id.trackerFragment)
-                R.id.menu_wiki -> navController.navigate(R.id.wikiFragment)
+                R.id.menu_statistic -> handleTabClick(item.itemId, R.id.statisticsFragment)
+                R.id.menu_tracker -> handleTabClick(item.itemId, R.id.trackerFragment)
+                R.id.menu_wiki -> handleTabClick(item.itemId, R.id.wikiFragment)
+                else -> return false
             }
 
             return true
         }
     }
 
+    private var destinationsHistory = HashMap<Int, Bundle>()
+
+    init {
+        destinationsHistory[R.id.menu_statistic] = Bundle()
+        destinationsHistory[R.id.menu_tracker] = Bundle()
+        destinationsHistory[R.id.menu_wiki] = Bundle()
+    }
+
+    private var currentTabId = R.id.menu_wiki
+
     private val destinationChangedListener: NavController.OnDestinationChangedListener =
         NavController.OnDestinationChangedListener { _, destination, _ ->
-            val destId = destination.id
-            if (destId != R.id.wikiFragment && destId != R.id.statisticsFragment && destId != R.id.trackerFragment) {
+            val oldDestId = destination.id
+            if (oldDestId != R.id.wikiFragment &&
+                oldDestId != R.id.statisticsFragment &&
+                oldDestId != R.id.trackerFragment
+            ) {
                 visibleToolbar.set(View.VISIBLE)
             } else {
                 visibleToolbar.set(View.GONE)
@@ -46,5 +61,44 @@ class HomeActivityViewModel(activity: Activity) : ViewModel() {
 
         if (destId != R.id.wikiFragment && destId != R.id.statisticsFragment && destId != R.id.trackerFragment)
             navController.popBackStack()
+    }
+
+    private fun handleTabClick(tabId: Int, hostFragmentId: Int) {
+        if(currentTabId != tabId) {
+            destinationsHistory[currentTabId] = navController.saveState() ?: Bundle()
+
+            navController.restoreState(destinationsHistory[tabId] ?: Bundle())
+            //todo some shit
+            navController.navigateUp()
+        }else {
+            destinationsHistory[tabId] = Bundle()
+            if (navController.currentDestination?.id != hostFragmentId) {
+                navController.navigate(hostFragmentId)
+            }
+        }
+        /*
+        if(currentTabId != tabId) {
+            if(navController.currentDestination?.id != hostFragmentId) {
+                destinationsHistory[currentTabId] = navController.saveState() ?: Bundle()
+            } else {
+                destinationsHistory[currentTabId] = Bundle()
+            }
+
+            val bundle = destinationsHistory[tabId] ?: Bundle()
+
+            if(!bundle.isEmpty) {
+                navController.restoreState(bundle)
+                navController.setGraph(R.navigation.main_graph)
+            } else {
+                navController.navigate(hostFragmentId)
+            }
+        } else {
+            destinationsHistory[tabId] = Bundle()
+            if (navController.currentDestination?.id != hostFragmentId) {
+                navController.navigate(hostFragmentId)
+            }
+        }
+*/
+        currentTabId = tabId
     }
 }
