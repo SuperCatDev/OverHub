db643448edcd2aca49cfecba7d5ff9b6022588bf
NTimchenko <ntimchenko@niisokb.ru>
2019-03-05 13:54:29 +0300

    Added image id binding

--- app/src/main/java/com/sc/overhub/bind/CustomViewBindings.kt
+++ app/src/main/java/com/sc/overhub/bind/CustomViewBindings.kt
@@ -1,12 +1,19 @@
 package com.sc.overhub.bind
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 @BindingAdapter("setAdapter")
 fun bindRecyclerViewAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>) {
     recyclerView.setHasFixedSize(true)
     recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
     recyclerView.adapter = adapter
+}
+
+@BindingAdapter("android:src")
+fun setImageDrawable(view: ImageView, resource: Int) {
+    view.setImageResource(resource)
 }
