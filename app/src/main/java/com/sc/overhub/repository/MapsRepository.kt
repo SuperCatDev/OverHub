2992006bd15f7d1759b440d6f0c23711e0e83b54
Alexey <ultimatum424@gmail.com>
2019-03-12 16:05:06 +0300

    Fix main thread problem

--- app/src/main/java/com/sc/overhub/repository/MapsRepository.kt
+++ app/src/main/java/com/sc/overhub/repository/MapsRepository.kt
@@ -6,29 +6,19 @@ import com.sc.overhub.data.WikiMapDao
 import com.sc.overhub.data.WikiMapEntry
 import com.sc.overhub.mapper.MapMapper
 import com.sc.overhub.model.GameMap
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 
 interface MapsRepository {
     suspend fun getMapsAsync(): Deferred<List<GameMap>>
 }
 
 class MapsRepositoryImpl (private val wikiMapDao: WikiMapDao, private val mapper: MapMapper): MapsRepository {
-    override suspend fun getMapsAsync(): Deferred<List<GameMap>> = coroutineScope {
+    override suspend fun getMapsAsync(): Deferred<List<GameMap>> = withContext(Dispatchers.IO) {
         async {
-            wikiMapDao.deleteAll() //СУКА МАИН ТХРЕАД111
-          //  wikiMapDao.insert(PREPOPULATE_DATA)
+            wikiMapDao.deleteAll() 
+            wikiMapDao.insert(PREPOPULATE_DATA)
             val q = wikiMapDao.getAll().map { mapper.mapTo(it) }
             return@async q
-            /*
-            listOf(GameMap("BLIZZARD WORLD", R.drawable.blizzard_world),
-                GameMap("NUMBANI", R.drawable.blizzard_world),
-                GameMap("ROAD 66", R.drawable.blizzard_world),
-                GameMap("LEIGHTZAN TOWER", R.drawable.blizzard_world),
-                GameMap("HOLLYWOOD", R.drawable.blizzard_world),
-                GameMap("ILIOS", R.drawable.blizzard_world))*/
         }
     }
 }
