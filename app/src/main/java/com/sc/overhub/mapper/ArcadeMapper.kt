38326c60996fad38dca4c6161aa81336fb564ad4
Alexey <ultimatum424@gmail.com>
2019-04-11 15:16:43 +0300

    New mapper for  ArcadeModeResponse to ArcadeModeEntity

--- app/src/main/java/com/sc/overhub/mapper/ArcadeMapper.kt
+++ app/src/main/java/com/sc/overhub/mapper/ArcadeMapper.kt
@@ -1,5 +1,6 @@
 package com.sc.overhub.mapper
 
+import com.sc.overhub.api.ArcadeModeResponse
 import com.sc.overhub.data.arcade.ArcadeModeEntity
 import com.sc.overhub.model.ArcadeModel
 import com.sc.overhub.model.ChangeTypeEnum
@@ -7,5 +8,9 @@ import com.sc.overhub.model.ChangeTypeEnum
 class ArcadeMapper {
     fun mapTo(data: ArcadeModeEntity, title: ChangeTypeEnum): ArcadeModel {
         return ArcadeModel(data.name, data.playersMode, data.imageLarge, data.imageNormal, title)
+    }
+
+    fun mapTo(data: ArcadeModeResponse): ArcadeModeEntity{
+        return ArcadeModeEntity(data.id, data.name, data.players, data.code, data.image.large, data.image.normal)
     }
 }
