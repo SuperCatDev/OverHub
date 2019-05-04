08e5ec86b5133c76d61cbd5c7bfd16ecc419658d
SuperCatDev <timchenkonikita123@gmail.com>
2019-05-05 00:18:34 +0300

    Added switch ranks image

--- app/src/main/java/com/sc/overhub/viewmodel/StatisticViewModel.kt
+++ app/src/main/java/com/sc/overhub/viewmodel/StatisticViewModel.kt
@@ -11,14 +11,35 @@ import org.koin.standalone.inject
 class StatisticViewModel : ScopedViewModel(), KoinComponent {
     private val repo: ProfileRepository by inject()
 
-    var score: ObservableField<String> = ObservableField(repo.getCashedScore())
+    var score: ObservableField<String>
     var nickname: ObservableField<String> = ObservableField(repo.getBattleTag())
-    var imageSrcId: ObservableInt = ObservableInt(R.drawable.diamond)
+    var imageSrcId: ObservableInt
 
     init {
+        val cashedScore = repo.getCashedScore()
+        score = ObservableField(cashedScore)
+        imageSrcId = ObservableInt(if (cashedScore.isNotEmpty()) getRankIdDependsOnScore(cashedScore) else R.drawable.gm)
+
         launch {
-            score.set(repo.getPlayerScore())
-            // add choosing image id depends on score
+            val scoreStr = repo.getPlayerScore()
+
+            if (scoreStr.isEmpty())
+                return@launch
+
+            score.set(scoreStr)
+
+            imageSrcId.set(getRankIdDependsOnScore(scoreStr))
         }
+    }
+
+    /** [score] must be int value */
+    private fun getRankIdDependsOnScore(score: String) = when (score.toInt()) {
+        in 0..1499 -> R.drawable.bronze
+        in 1500..1999 -> R.drawable.silver
+        in 2000..2499 -> R.drawable.gold
+        in 2500..2999 -> R.drawable.platinum
+        in 3000..3499 -> R.drawable.diamond
+        in 3500..3999 -> R.drawable.master
+        else -> R.drawable.gm
     }
 }
