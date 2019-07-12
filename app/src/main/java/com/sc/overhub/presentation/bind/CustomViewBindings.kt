3b35bf26fc7670aad113f63da987f20507161595
Nikita <timchenkonikita123@gmail.com>
2019-07-12 12:49:31 +0300

    Removed unused binding method

--- app/src/main/java/com/sc/overhub/presentation/bind/CustomViewBindings.kt
+++ app/src/main/java/com/sc/overhub/presentation/bind/CustomViewBindings.kt
@@ -2,22 +2,10 @@ package com.sc.overhub.presentation.bind
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-
-@BindingAdapter("setAdapter")
-fun bindRecyclerViewAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>?) {
-    if (adapter == null) return
-
-    recyclerView.setHasFixedSize(true)
-    recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
-    recyclerView.adapter = adapter
-}
-
 @BindingAdapter("android:src")
-fun setImageDrawable(view: ImageView, resource: Int?)  =
+fun setImageDrawable(view: ImageView, resource: Int?) =
     Glide.with(view)
         .load(resource)
         .fitCenter()
