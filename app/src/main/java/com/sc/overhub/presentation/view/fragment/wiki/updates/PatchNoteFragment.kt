d0e3f72bc2f75b2703be1ad818dcb532833dd4c8
Nikita <timchenkonikita123@gmail.com>
2019-09-06 19:38:43 +0300

    fragment added

--- app/src/main/java/com/sc/overhub/presentation/view/fragment/wiki/updates/PatchNoteFragment.kt
+++ app/src/main/java/com/sc/overhub/presentation/view/fragment/wiki/updates/PatchNoteFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.sc.overhub.R
+import com.sc.overhub.databinding.FragmentWikiUpdatesBinding
 import com.sc.overhub.presentation.view.fragment.BaseFragment
 import com.sc.overhub.presentation.viewmodel.PatchNoteViewModel
 import com.sc.overhub.presentation.viewmodel.getViewModel
@@ -15,8 +18,17 @@ class PatchNoteFragment : BaseFragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding: FragmentWikiUpdatesBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_wiki_updates, container, false
+        )
 
+        fragmentBinding.viewModel = viewModel
+
+        return fragmentBinding.root
     }
 }
